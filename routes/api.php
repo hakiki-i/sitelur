597ab2122fd78e69ed0f037175f3542565bed7d2
hakiki-i <nugrohohakiki35@gmail.com>
2026-05-07 16:39:14 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -10,24 +10,15 @@ use App\Http\Controllers\DashboardController;
 // LOGIN
 Route::post('/login', [ApiLoginController::class, 'login']);
 
-// TEST
 Route::get('/test', function () {
     return response()->json([
         'status' => true,
         'message' => 'API OK'
     ]);
 });
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'apiIndex']);
-    // ...route lain
-});
-// ROUTE LOGIN
-Route::middleware('auth:sanctum')->group(function () {
-
-    Route::get('/dashboard', [DashboardController::class, 'apiIndex']);
-
     Route::get('/kandang', [KandangController::class, 'apiIndex']);
-
     Route::post('/produksi', [ProduksiController::class, 'apiStore']);
-
 });
